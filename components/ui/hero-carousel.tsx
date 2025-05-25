086acf7a1ceb30a0ecfbd6d6d1d3c8605bdce87a
linseanyu@gmail.com <linseanyu@gmail.com>
2025-05-25 10:33:50 +0800

    fix deployment error

--- components/ui/hero-carousel.tsx
+++ components/ui/hero-carousel.tsx
@@ -44,14 +44,12 @@ export function HeroCarousel({
 }: HeroCarouselProps) {
   const [api, setApi] = useState<CarouselApi>()
   const [current, setCurrent] = useState(0)
-  const [count, setCount] = useState(0)
 
   useEffect(() => {
     if (!api) {
       return
     }
 
-    setCount(api.scrollSnapList().length)
     setCurrent(api.selectedScrollSnap() + 1)
 
     api.on("select", () => {
